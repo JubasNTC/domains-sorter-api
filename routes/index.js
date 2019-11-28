0001bfe08e775192dbb4416d0d5a689fe391ff7a
JubasNTC <jubas1337@mail.ru>
2019-11-28 15:10:53 +0300

    add method remove dublicates

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,48 @@
 const express = require('express');
-const router = express.Router();
 const multiparty = require('multiparty');
+const es = require('event-stream');
 const fs = require('fs');
-const readline = require('readline');
-const Stream = require('stream');
 
+const router = express.Router();
 const Sorter = require('../models/Sorter');
 
 router.get('/', (req, res) => {
-  res.send({ status: 200 });
+  res.status(200);
+  res.send({ result: 'server is working...' });
 });
 
 router.post('/sort-domains', (req, res) => {
   const form = new multiparty.Form();
-  // eslint-disable-next-line handle-callback-err
+
   form.parse(req, (error, fields, files) => {
+    if (error) {
+      res.status(400);
+      res.send({ error: error });
+    }
+
     const sorter = new Sorter();
-    const instream = fs.createReadStream(files.file[0].path);
-    const outstream = new Stream();
-    const rl = readline.createInterface(instream, outstream);
-
-    rl.on('line', line => {
-      sorter.addLine(line);
-    });
-
-    rl.on('close', () => {
-      res.send({ status: 200, result: sorter.getSortResult() });
-    });
+    const fileStream = fs
+      .createReadStream(files.file[0].path)
+      .pipe(es.split())
+      .pipe(
+        es
+          .mapSync(line => {
+            fileStream.pause();
+            sorter.addLine(line);
+            fileStream.resume();
+          })
+          .on('error', err => {
+            res.status(500);
+            res.send({ error: err });
+          })
+          .on('close', () => {
+            fileStream.destroy();
+          })
+          .on('end', () => {
+            res.status(200);
+            res.send({ result: sorter.getSortResult() });
+          })
+      );
   });
 });
 
